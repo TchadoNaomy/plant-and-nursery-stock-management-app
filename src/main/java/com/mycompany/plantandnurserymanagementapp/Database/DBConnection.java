39112c25733b40a0a29629f84546f0fc0d644006
TchadoNaomy <tchadonaomi470@gmail.com>
2025-02-14 19:13:25 +0100

    Update DBConnection.java

--- src/main/java/com/mycompany/plantandnurserymanagementapp/Database/DBConnection.java
+++ src/main/java/com/mycompany/plantandnurserymanagementapp/Database/DBConnection.java
@@ -4,10 +4,7 @@
  */
 package com.mycompany.plantandnurserymanagementapp.Database;
 
-/**
- *
- * @author Kapnang
- */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
